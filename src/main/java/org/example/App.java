9084cf1429dc3a517414c9940a7d24f2cdad47e0
Nataliia Myzuchenko <168213362+NataliaMuza@users.noreply.github.com>
2024-05-18 19:19:51 +0300

    Add files via upload

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class App
+{
+  public static void main(String[] args)
+  //{
+  //  Automobile myAutomobile = new Automobile( "Kia", 2017);
+  //  System.out.println(myAutomobile.getModel()+myAutomobile.getRik());
+  //}
+  //{
+  //  Automobile myAutomobile = new Automobile( "Kia", 2017);
+  //  System.out.printf(String.format("model: %s, rik: %s", myAutomobile.getModel(),myAutomobile.getRik()));
+  //}
+  //{
+  //  Automobile myAutomobile = new Automobile( "Kia", 2017);
+  //  System.out.printf(String.format("--- %s ---", myAutomobile.getModel()));
+  //}
+  {
+      Automobile myAutomobile = new Automobile( "Kia", 2017);
+      System.out.printf(String.format("~~~ %s ~~~", myAutomobile.getRik()));
+  }
+
+}
+
+
